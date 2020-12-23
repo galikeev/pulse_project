1ae2c2085b1762e4453a3d4649b0c15f4b2c6983
Ramil <vk2011@bk.ru>
2020-12-23 22:55:08 +0500

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,11 @@ $(document).ready(function(){
 
     $('form').submit(function(e) {
         e.preventDefault();
+        if (!$(this).valid()) {
+
+            return;
+            
+            };
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
